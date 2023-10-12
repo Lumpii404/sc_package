b5c94d1caf22328b6fd00950fbf5d07f9011e549
Scube Scripts <104854776+ScubeScripts@users.noreply.github.com>
2023-10-12 17:18:35 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,7 @@
+ESX = exports["es_extended"]:getSharedObject()
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+  PlayerData = xPlayer
+end)
+ 
---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "sc_package"
+author "Scube Scripts"
+version "1.0.1"
+description "This script allows you to create packages and get any items from them."
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
+ 
