589e9f4eeda03624b0007c60b35880d617ad9114
Scube Scripts <104854776+ScubeScripts@users.noreply.github.com>
2023-10-14 18:47:16 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,8 @@ game 'gta5'
 
 name 'sc_package'
 author 'Scube Scripts'
-version '1.0.1'
 description 'This script allows you to create packages and get any items from them.'
+version '1.0.1'
 
 client_scripts {
     'config.lua',
